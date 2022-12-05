33894b993c9ef8806068f7963063781563107fa5
JaeHoon-Shin <sinhoon0805@naver.com>
2022-12-05 18:29:10 +0900

    반응형작업

--- js/common.js
+++ js/common.js
@@ -2,7 +2,7 @@
 function side (){
     let i = window.pageYOffset
     const nonSide = document.querySelector('.side-bar')
-    i>0 ? nonSide.style.opacity ="1" : nonSide.style.opacity ="0" ;
+    i>0 ? nonSide.style.opacity = "1" : nonSide.style.opacity = "0" ;
 }
 
 window.addEventListener('scroll',side);
