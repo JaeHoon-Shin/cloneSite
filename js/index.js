fe5ddfac5910d74e9479ec0caf343f370212ae98
JaeHoon-Shin <sinhoon0805@naver.com>
2022-12-06 09:40:09 +0900

    반응형 완성

--- js/index.js
+++ js/index.js
@@ -31,24 +31,5 @@ licenseBox.style.display = 'none';
 document.querySelector('html').style.overflow = 'visible';
 };
 
-const tabBtn = document.querySelector('.all-cate>a'),
-      tabBox = document.querySelector('.tab-container'),
-      tabSubBtn = document.querySelectorAll('.tab-main-list>ul>li');
-      tabSubList = document.querySelectorAll('.sub-tab-list>ul')
-      tabBanner = document.querySelectorAll('.tab-banner');
-tabBtn.onclick = function(){
-    tabBox.classList.toggle('on');
-};
-let idx =0;
-tabSubBtn.forEach(function(el,key){
-    el.onclick=function(){
-        tabSubList[idx].classList.remove('on');
-        tabBanner[idx].classList.remove('on');
-        tabSubList[key].classList.toggle('on');
-        tabBanner[key].classList.toggle('on');
-        idx = key;
-        console.log(key)
-    };
 
-});
 
