0bd4f3fd12568158d93dc530e96e4ea49c312074
JaeHoon-Shin <sinhoon0805@naver.com>
2022-12-15 18:19:57 +0900

    슬라이드 슬릭으로 변환

--- js/common.js
+++ js/common.js
@@ -1,59 +1,60 @@
 $('body').prepend('<header>');
 $('body').append('<footer>');
-$('header').load('./inc.html header',head);
-$('footer').load('./inc.html footer',foot);
-function head(){
-function side (){
-    let i = window.pageYOffset
-    const nonSide = document.querySelector('.side-bar')
-    i>0 ? nonSide.style.opacity = "1" : nonSide.style.opacity = "0" ;
-}
-window.addEventListener('scroll',side);
+$('header').load('./inc.html header', head);
+$('footer').load('./inc.html footer', foot);
+function head() {
+    window.addEventListener('scroll', side);
+    //사이드바
+    function side() {
+        let i = window.pageYOffset
+        const nonSide = document.querySelector('.side-bar')
+        i > 0 ? nonSide.style.opacity = "1" : nonSide.style.opacity = "0";
+    }
 
-let hidx = localStorage.idx;
-$('.header-top-menu>ul>li').removeClass('on');
-$('.header-top-menu>ul>li').eq(hidx).addClass('on');
-$('.header-top-menu>ul>li').on('click', function(){
-    hidx = $(this).index();
-    localStorage.hidx = hidx;   
-});
-localStorage.clear();
-
-const tabBtn = document.querySelector('.all-cate>a'),
-      tabBox = document.querySelector('.tab-container'),
-      tabSubBtn = document.querySelectorAll('.tab-main-list>ul>li');
-      tabSubList = document.querySelectorAll('.sub-tab-list>ul')
-      tabBanner = document.querySelectorAll('.tab-banner');
-tabBtn.onclick = function(){
-    tabBox.classList.toggle('on');
-};
-let idx =0;
-tabSubBtn.forEach(function(el,key){
-    el.onclick=function(){
-        tabSubList[idx].classList.remove('on');
-        tabBanner[idx].classList.remove('on');
-        tabSubList[key].classList.toggle('on');
-        tabBanner[key].classList.toggle('on');
-        idx = key;
-        console.log(key)
+    //헤더 메뉴 클릭시 위치 확인
+    let hidx = localStorage.idx;
+    $('.header-top-menu>ul>li').removeClass('on');
+    $('.header-top-menu>ul>li').eq(hidx).addClass('on');
+    $('.header-top-menu>ul>li').on('click', function () {
+        hidx = $(this).index();
+        localStorage.hidx = hidx;
+    });
+    localStorage.clear();
+    //헤더 메뉴 바 
+    const tabBtn = document.querySelector('.all-cate>a'),
+        tabBox = document.querySelector('.tab-container'),
+        tabSubBtn = document.querySelectorAll('.tab-main-list>ul>li');
+    tabSubList = document.querySelectorAll('.sub-tab-list>ul')
+    tabBanner = document.querySelectorAll('.tab-banner');
+    tabBtn.onclick = function () {
+        tabBox.classList.toggle('on');
     };
-
-});
+    let idx = 0;
+    tabSubBtn.forEach(function (el, key) {
+        el.onclick = function () {
+            tabSubList[idx].classList.remove('on');
+            tabBanner[idx].classList.remove('on');
+            tabSubList[key].classList.toggle('on');
+            tabBanner[key].classList.toggle('on');
+            idx = key;
+            console.log(key)
+        };
+    });
 }
-function foot(){
+function foot() {
     const licenseBtn = document.querySelector('.license-btn'),
-licenseBox = document.querySelector('.license-dark');
+        licenseBox = document.querySelector('.license-dark');
 
-licenseBtn.onclick = function () {
-licenseBox.style.display = 'block';
-document.querySelector('html').style.overflow = 'hidden';
-};
-document.querySelector('.close').onclick = function () {
-licenseBox.style.display = 'none';
-document.querySelector('html').style.overflow = 'visible';
-};
-$('.top-icon').on('click',function(e){
-    e.preventDefault(); /* e.preventDefault() */
-    window.scrollTo(0,0);
-})
+    licenseBtn.onclick = function () {
+        licenseBox.style.display = 'block';
+        document.querySelector('html').style.overflow = 'hidden';
+    };
+    document.querySelector('.close').onclick = function () {
+        licenseBox.style.display = 'none';
+        document.querySelector('html').style.overflow = 'visible';
+    };
+    $('.top-icon').on('click', function (e) {
+        e.preventDefault(); /* e.preventDefault() */
+        window.scrollTo(0, 0);
+    })
 }
--- js/index.js
+++ js/index.js
@@ -1,59 +1,101 @@
 
-
+$(document).ready(function () {
+    slickSlide();
+});
 if (document.cookie.match('팝업')) {
     document.querySelector('.popup-background').style.display = 'none';
 }
 else {
     document.querySelector('.popup-background').style.display = 'block';
-    popSlide();
- /*    $('.popup-slide').resize(); // 팝업 열때 슬라이드 깨짐 방지
-    $('.popup-slide').slick('refresh'); // 팝업 열때 슬라이드 깨짐 방지 */
 }
+//팝업 이벤트 
 const elPopUpClose = document.querySelectorAll('div.popup-text-container > div > a');
 elPopUpClose.forEach(function (a, key) {
     a.onclick = function () {
         if (key == 0) {
             console.log(key)
             let toDay = new Date();
-            toDay.setSeconds(toDay.getSeconds() + 10);
+            toDay.setMinutes(toDay.getMinutes() + 10);
             document.cookie = `popup=팝업;expires=${toDay.toUTCString()}`
-
-            /* let today = new Date();
-            today.setSeconds(today.getSeconds() + 30);
-            document.cookie = `popup=가격할인이벤트!;expires=${today.toUTCString()}` */
         }
         document.querySelector('.popup-background').style.display = 'none';
     };
 });
-
-function popSlide(){
+//리뷰 슬라이드 
+const reviewBtn = document.querySelectorAll('.review-container > .visual-box > button'),
+    reviewBox = document.querySelector('.review-list');
+let start = 0;
+reviewBtn.forEach(function (btn, key) {
+    btn.addEventListener('click', function () {
+        if (start < 3) {
+            if (key == 0) {
+                start++
+            }
+        }
+        if (start > 0) {
+            if (key == 1) {
+                start--
+            }
+        }
+        reviewBox.style = `transform : translateX(-${350 * start}px);`
+    })
+})
+//슬릭 슬라이드 함수 
+function slickSlide() {
     $('.popup-slide').slick({
-        slide: 'div',
-        slidesToShow: 1,        // 한 화면에 보여질 컨텐츠 개수
-      arrow: true,
-      setPosition: 0
+        slidesToShow: 1,         // 한 화면에 보여질 컨텐츠 개수
+        arrows: false,           // true 이전 다음 버튼 생성 false 생성 X
+        infinite: true,         // 무제한으로 슬라이드 되는지
+        autoplay: true,           // 페이지 넘기기 자동 false 시 넘어가지 않음 
+        autoplaySpeed: 5000,    // 자동 스크롤 넘어가는 시간 	
+        setPosition: 0,
+        dots: true,
+        dotsClass: "slick-dots"
     });
-    /*          //슬라이드 되어야 할 태그
-      infinite: true,     //무한 반복 옵션     
-      slidesToShow: 1,        // 한 화면에 보여질 컨텐츠 개수
-      arrow: true,
-      setPosition: 0,
-  }) */
+    $('.img-box-list').slick({
+        slidesToShow: 1,         // 한 화면에 보여질 컨텐츠 개수
+        arrows: true,           // true 이전 다음 버튼 생성 false 생성 X
+        infinite: true,         // 무제한으로 슬라이드 되는지
+        autoplay: true,           // 페이지 넘기기 자동 false 시 넘어가지 않음 
+        autoplaySpeed: 3000,    // 자동 스크롤 넘어가는 시간 	
+        setPosition: 0,
+        dots: true,
+        fade: true,
+        dotsClass: "view-slick-dots"
+    });
+    $('.celeb-visual-list').slick({
+        slide: 'div',
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        responsive:  // 반응형 작업
+        [
+            {
+                breakpoint: 760,   //width 1024 이상부터
+                settings: 
+                {
+                    slidesToShow: 2
+                }
+            }
+        ]
+    });
+    $('.notice-list-box').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: true,           // true 이전 다음 버튼 생성 false 생성 X
+        infinite: true,  
+        vertical : true,
+        verticalSwiping: true
+    })
 }
-let mainViewNum = 0;
-function mainSlideFun(n) {
-    mainViewAll[n].style = "left:" + ((-100) * n) + "%; opacity:1";
-}
-let mainViewAll = document.querySelectorAll(`main>div.visual-box>div.img-box-list>div.img-box`);
-let mainViewAni = setInterval(function () {
-    ++mainViewNum;
-    /* let numPx = (-1903) * Number(mainViewNum); */
-    mainSlideFun(mainViewNum);
-    if (mainViewNum > 9) {
-        mainViewAni = 0;
-    }
-}, 8000);
-
+$('.youtube-play-btn').on('click',function(e){
+    e.preventDefault();
+    $(".youtube-play>iframe")[0].contentWindow.postMessage('{"event":"command","func":"' + 'playVideo' + '","args":""}',
+    '*'); 
+})
 
 
 
